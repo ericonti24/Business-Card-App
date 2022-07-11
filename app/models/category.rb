47e8530e021dbde3aa9533ad53b90895b0edc33a
Eric <ericontiveros123@gmail.com>
2022-07-11 11:51:44 -0700

    crud

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 class Category < ApplicationRecord
     has_many :business_cards
+    has_many :users, through: :business_cards
+
+    validates :name, presence: true
 
     scope :filter_duplicates, -> { group(:name).having("count(*) >= 1")}
 
